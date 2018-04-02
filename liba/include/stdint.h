616ce9dc8713d5bf9a2932a6321129711ccbb7d9
Zhaofeng Li <hello@zhaofeng.li>
2018-04-02 23:54:40 +0000

    [liba] Add limit macros to stdint.h

--- liba/include/stdint.h
+++ liba/include/stdint.h
@@ -1,6 +1,8 @@
 #ifndef LIBA_STDINT_H
 #define LIBA_STDINT_H
 
+#include <limits.h>
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -25,8 +27,59 @@ typedef int32_t int_fast32_t;
 typedef int64_t int_fast64_t;
 
 typedef uint8_t uint_least8_t;
+typedef uint16_t uint_least16_t;
+typedef uint32_t uint_least32_t;
+typedef uint64_t uint_least64_t;
+
+typedef int8_t int_least8_t;
+typedef int16_t int_least16_t;
+typedef int32_t int_least32_t;
+typedef int64_t int_least64_t;
+
+typedef uint64_t uintmax_t;
+typedef int64_t intmax_t;
+
+typedef int32_t ptrdiff_t;
 
 #define UINT64_C(c) c ## ULL
 #define INT64_C(c) c ## LL
+
+/* Limits of integral types. */
+#ifndef INT8_MIN
+#define INT8_MIN               (-128)
+#endif
+#ifndef INT16_MIN
+#define INT16_MIN              (-32767-1)
+#endif
+#ifndef INT32_MIN
+#define INT32_MIN              (-2147483647-1)
+#endif
+#ifndef INT8_MAX
+#define INT8_MAX               (127)
+#endif
+#ifndef INT16_MAX
+#define INT16_MAX              (32767)
+#endif
+#ifndef INT32_MAX
+#define INT32_MAX              (2147483647)
+#endif
+#ifndef UINT8_MAX
+#define UINT8_MAX              (255U)
+#endif
+#ifndef UINT16_MAX
+#define UINT16_MAX             (65535U)
+#endif
+#ifndef UINT32_MAX
+#define UINT32_MAX             (4294967295U)
+#endif
+
+#ifndef SIZE_MAX
+#define SIZE_MAX               UINT_MAX
+#endif
+
+#define UINT_FAST32_MAX UINT32_MAX
+
+#define INT_FAST32_MIN INT32_MIN
+#define INT_FAST32_MAX INT32_MAX
 
 #endif
