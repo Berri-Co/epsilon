1fc696024ec0a0b2025fdcda71b04f30c8a55f9d
Zhaofeng Li <hello@zhaofeng.li>
2018-04-04 03:46:39 +0000

    [liba] Add snprintf() and vsnprintf()

--- liba/include/stdio.h
+++ liba/include/stdio.h
@@ -1,10 +1,17 @@
 #ifndef LIBA_STDIO_H
 #define LIBA_STDIO_H
 
+#include <stddef.h>
+#include <stdarg.h>
+#include <printf.h>
+
 #define stdin 0
 #define stdout 0
 #define EOF 0
 
 typedef void FILE;
+
+#define snprintf(s, n, format, ...) sprintf(s, format, __VA_ARGS__)
+#define vsnprintf(s, n, format, arg) vsprintf(s, format, arg)
 
 #endif
